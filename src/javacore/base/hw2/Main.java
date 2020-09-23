a34cd0e0eb3eea82585852c6bbc79f1455dc1dba
vkharapaev <vkharapaev@gmail.com>
2020-09-24 02:21:42 +0500

    incomplete 1

--- src/javacore/base/hw2/Main.java
+++ src/javacore/base/hw2/Main.java
@@ -1,7 +1,74 @@
 package javacore.base.hw2;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
 
+    // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+    // С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    private static void task1() {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] == 0 ? 1 : 0;
+        }
+        System.out.printf("task1: %s\n", Arrays.toString(arr));
+    }
+
+    // 2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+    private static void task2() {
+        int[] arr = new int[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * 3;
+        }
+        System.out.printf("task2: %s\n", Arrays.toString(arr));
+    }
+
+    // 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+    private static void task3() {
+        int arr[] = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
+            }
+        }
+        System.out.printf("task3: %s\n", Arrays.toString(arr));
+    }
+
+    // 4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью
+    // цикла(-ов) заполнить его диагональные элементы единицами;
+    private static void task4() {
+        int arr[][] = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0},
+        };
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            arr[i][i] = 1;
+            arr[len - 1 - i][i] = 1;
+        }
+        System.out.printf("task4: %s\n", Arrays.deepToString(arr));
+    }
+
+    // 5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
+    private static void task5() {
+        int arr[] = {2, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int min = arr[0], max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.printf("task5: min = %d, max = %d\n", min, max);
     }
 }
