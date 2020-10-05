6394f64504e53f946be30c8e9f56d18df14d882c
vkharapaev <vkharapaev@gmail.com>
2020-10-06 01:16:53 +0500

    hw5 was done

---
+++ src/javacore/base/hw5/Person.java
@@ -0,0 +1,80 @@
+package javacore.base.hw5;
+
+public class Person {
+
+    private String fio;
+    private String position;
+    private String email;
+    private String phone;
+    private float salary;
+    private int age;
+
+    public Person(String fio, String position, String email, String phone, float salary, int age) {
+        this.fio = fio;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fio='" + fio + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+}
