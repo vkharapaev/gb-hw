f97158081dbf5b93854c5e835bfa11c39fde8803
Vladimir Kharapaev <vkharapaev@gmail.com>
2020-11-10 03:37:29 +0500

    hw3

---
+++ src/javacore/advanced/hw3/task1/Main.java
@@ -0,0 +1,30 @@
+package javacore.advanced.hw3.task1;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] wordList = {"linux", "began", "in", "as", "personal", "project", "by", "finnish", "student",
+                "linus", "torvalds", "to", "create", "a",
+                "linus", "torvalds", "to", "create", "a",};
+
+        Map<String, Integer> wordFreqMap = new LinkedHashMap<>();
+        for (String word : wordList) {
+            wordFreqMap.compute(word, (k, v) -> v != null ? v + 1 : 1);
+        }
+
+        System.out.println("Unique words:");
+        for (Map.Entry<String, Integer> entry : wordFreqMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                System.out.printf(" - %s\n", entry.getKey());
+            }
+        }
+
+        System.out.println("Word frequency:");
+        for (Map.Entry<String, Integer> entry : wordFreqMap.entrySet()) {
+            System.out.printf(" - %d: %s \n", entry.getValue(), entry.getKey());
+        }
+    }
+
+}
---
+++ src/javacore/advanced/hw3/task2/Phonebook.java
@@ -0,0 +1,37 @@
+package javacore.advanced.hw3.task2;
+
+import java.util.*;
+
+public class Phonebook {
+
+    private final Map<String, Set<String>> data;
+
+    public Phonebook() {
+        data = new HashMap<>();
+    }
+
+    /**
+     * Add a phone number.
+     *
+     * @param name  Person last name
+     * @param phone Phone number
+     */
+    public void add(String name, String phone) {
+        data.computeIfAbsent(normalize(name), key -> new HashSet<>())
+                .add(phone);
+    }
+
+    /**
+     * Get phone numbers by a person last name.
+     *
+     * @param name Person last name
+     * @return Collection of phone numbers that belong to the person
+     */
+    public Collection<String> get(String name) {
+        return Collections.unmodifiableSet(data.getOrDefault(normalize(name), Collections.emptySet()));
+    }
+
+    private String normalize(String name) {
+        return name.toLowerCase().trim();
+    }
+}
