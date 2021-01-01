8a7651f37c57a00eec5457ce6a710bbfdf6c0975
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-01-02 02:18:17 +0500

    hw5

--- src/javacore/professional/hw5/MainClass.java
+++ src/javacore/professional/hw5/MainClass.java
@@ -2,11 +2,10 @@ package javacore.professional.hw5;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static final int HALF_CARS_COUNT = (int) (CARS_COUNT * 0.5f);
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(CARS_COUNT, new Road(60), new Tunnel(HALF_CARS_COUNT), new Road(40));
+        Race race = new Race(CARS_COUNT, new Road(60), new Tunnel(CARS_COUNT / 2), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
